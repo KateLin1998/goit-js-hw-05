2c0140e11fbc189487efc82c3a37662c2a4f50b2
Kateryna <katarina144144@gmail.com>
2025-04-28 17:30:09 +0300

    ...

--- js/task-4.js
+++ js/task-4.js
@@ -1,28 +1,50 @@
-const profile = {
-    username: "Jacob",
-  playTime: 300,
+function getTotalBalanceByGender(users, gender) {
+  return users
+    .filter(function(user) {
+      return user.gender === gender;
+    })
+    .reduce(function(total, user) {
+      return total + user.balance;
+    }, 0);
+}
 
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
 
-changeUsername(newName) {
-  this.username = newName;
-  return `${this.username} has ${this.playTime} active hours!`;
-},
-
-updatePlayTime(playTime) {
-  this.playTime += playTime; 
-},
-
-  getInfo() {
-  return `${this.username} has ${this.playTime} active hours!`
-},
-
-};
-
-
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+console.log(getTotalBalanceByGender(clients, "male"));   // 12053
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
